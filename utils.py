a1459c8d1ac28ef5e71bed9aa31910e3b8deb0c2
halil-ibrahim <halil-ibo1510@hotmail.com>
2019-02-14 11:02:16 +0100

    code des tests et fonctions

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
+from math import sqrt, sin ,cos,pi
+from scipy.integrate import quad
 
 def fact(n):
 	"""Computes the factorial of a natural number.
@@ -10,6 +12,14 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
+	if n<0:
+		raise ValueError("must be >=0")
+	if n ==0:
+		return 1
+	facto = 1
+	for i in range(1,n+1):
+		facto=facto*i
+	return facto
 	pass
 
 def roots(a, b, c):
@@ -19,7 +29,18 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	racine = ()
+	delta=(b*b)-4*a*c
+	if delta>0:
+		racine=((((-b)+sqrt(delta))/(2*a)),(((-b)-sqrt(delta))/(2*a)))
+		return racine
+	if delta==0:
+		racine=((-b)/(2*a))
+		return racine
+	if delta<0:
+		raise ValueError("delta est négatif")
+		return racine 
+
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -34,9 +55,15 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+
+	def f(x):
+		return eval(function)
+
+	I = quad(f,lower,upper)
+	return I[0]
+	
 
 if __name__ == '__main__':
 	print(fact(5))
-	print(roots(1, 0, 1))
+	print(roots(1, 3, 2))
 	print(integrate('x ** 2 - 1', -1, 1))
