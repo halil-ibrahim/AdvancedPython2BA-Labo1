e5efca0d7e74fe5e23e87e31b0ee619b61104f94
halil-ibrahim <halil-ibo1510@hotmail.com>
2019-02-14 11:02:46 +0100

    fin des test 2

--- test_utils.py
+++ test_utils.py
@@ -8,15 +8,22 @@ import utils
 class TestUtils(unittest.TestCase):
     def test_fact(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.fact(5), 120)
+        with self.assertRaises(ValueError):
+            utils.fact(-5)
     
     def test_roots(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.roots(1,3,2),(-1.0,-2.0))
+        with self.assertRaises(ValueError):
+            utils.roots(1,0,-1)
+      
     
     def test_integrate(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.integrate('x ** 2 - 1',-1,1),-1.3333333333333335)
+
+        
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
